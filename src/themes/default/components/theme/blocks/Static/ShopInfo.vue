32dcbf4a6ba68cf98718b95c0f2db0cf6c791c52
przemek-mielcarz <przemek@mielcarz.eu>
2019-12-28 13:48:55 +0100

    main page

---
+++ src/themes/default/components/theme/blocks/Static/ShopInfo.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="shop-info">
+    <div class="container">
+      <ul>
+        <li class="delivery">
+          Dostawa Gratis
+          <br>
+          <span>od 300 zł brutto</span>
+        </li>
+        <li class="return">
+          30 dni na zwrot
+          <br>
+          <span>Bez podania przyczyny</span>
+        </li>
+        <li class="warranty">
+          Garancja oryginalności
+          <br>
+          <span>Sprzedajemy tylko oryginalne produkty</span>
+        </li>
+        <li class="security">
+          Bezpieczne zakupy
+          <br>
+          <span>Ochrona do 10 tys. zł i min. 2 lata gwarancji</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.shop-info {
+  background: #f7f9fc;
+  border-top: solid 1px #cbd3dd;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    margin: 0px;
+    padding: 0px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    li {
+      font-size: 18px;
+      font-weight: normal;
+      line-height: 18px;
+      color: #000000;
+      padding-right: 0px;
+      position: relative;
+      display: block;
+      padding-top: 10px;
+      padding-bottom: 10px;
+      box-sizing: border-box;
+      span {
+        font-size: 14px;
+        line-height: 18px;
+      }
+      &:before {
+        position: absolute;
+        content: "";
+        display: block;
+      }
+      &.delivery {
+        padding-left: 80px;
+        &:before {
+          position: absolute;
+          background: url("/assets/atat/delivery.svg") no-repeat;
+          background-size: cover;
+          width: 68px;
+          height: 36px;
+          top: 0px;
+          bottom: 0px;
+          left: 0px;
+          margin: auto;
+        }
+      }
+      &.return {
+        padding-left: 80px;
+        &:before {
+          position: absolute;
+          background: url("/assets/atat/return.svg") no-repeat;
+          background-size: cover;
+          width: 27px;
+          height: 29px;
+          top: 0px;
+          bottom: 0px;
+          left: 40px;
+          margin: auto;
+        }
+      }
+      &.warranty {
+        padding-left: 80px;
+        &:before {
+          position: absolute;
+          background: url("/assets/atat/warranty.svg") no-repeat;
+          background-size: cover;
+          width: 29px;
+          height: 36px;
+          top: 0px;
+          bottom: 0px;
+          left: 40px;
+          margin: auto;
+        }
+      }
+      &.security {
+        padding-left: 80px;
+        &:before {
+          position: absolute;
+          background: url("/assets/atat/security.svg") no-repeat;
+          background-size: cover;
+          width: 32px;
+          height: 35px;
+          top: 0px;
+          bottom: 0px;
+          left: 40px;
+          margin: auto;
+        }
+      }
+      @media (max-width: 1200px) {
+        width: 35%;
+      }
+      @media (max-width: 992px) {
+        width: 50%;
+      }
+      @media (max-width: 576px) {
+        width: 80%;
+      }
+    }
+  }
+}
+</style>
--- src/themes/default/pages/StartPage.vue
+++ src/themes/default/pages/StartPage.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-<<<<<<< HEAD
     <header-simple />
     <shop-info />
+    <main-slider />
     <div class="container main_category">
       <div class="flex main_category__boxes between-xs">
         <router-link
-          :to="localizedRoute('/c/elektryka')"
-          class="main_category__elem  main_category__elem--electric"
+          :to="localizedRoute('/elektryka')"
+          class="main_category__elem main_category__elem--electric"
         >
           <h3 class="main_category__title icon icon--electric">
             {{ $t("Electricity") }}
           </h3>
         </router-link>
         <router-link
-          :to="localizedRoute('/c/oprawy-oswietleniowe-konsumenckie')"
+          :to="localizedRoute('/elektryka/oprawy-oswietleniowe-konsumenckie')"
           class="main_category__elem main_category__elem--lights"
         >
           <h3 class="main_category__title icon icon--lights">
@@ -22,7 +22,7 @@
           </h3>
         </router-link>
         <router-link
-          :to="localizedRoute('/c/chemia')"
+          :to="localizedRoute('/chemia')"
           class="main_category__elem main_category__elem--chemistry"
         >
           <h3 class="main_category__title icon icon--chemistry">
@@ -30,72 +30,104 @@
           </h3>
         </router-link>
         <router-link
-          :to="localizedRoute('/c/spozywcze')"
+          :to="localizedRoute('/spozywcze')"
           class="main_category__elem main_category__elem--food"
         >
           <h3 class="main_category__title icon icon--food">
             {{ $t("Food") }}
           </h3>
         </router-link>
-=======
-    <div class="bg-cl-secondary py35 pl20">
-      <div class="container">
-        <h2 class="fs-big">
-          {{ $t('Atat start page') }}
-        </h2>
       </div>
     </div>
-
-    <div class="container pt45 pb70">
-      <h3>{{ $t('Choose store industry') }}</h3>
-      <div class="row pl20 pt0">
-        <div class="col-sm-3">
-          <nav class="static-menu serif h4 mb35">
-            <ul class="m0 p0">
-              <li class="mb10">
-                <router-link :to="localizedRoute('/elektryka')" class="cl-accent relative">
-                  {{ $t('Electricity') }}
-                </router-link>
-              </li>
-              <li class="mb10">
-                <router-link :to="localizedRoute('/elektryka/oprawy-oswietleniowe-konsumenckie')" class="cl-accent relative">
-                  {{ $t('Lighting') }}
-                </router-link>
-              </li>
-              <li class="mb10">
-                <router-link :to="localizedRoute('/chemia')" class="cl-accent relative">
-                  {{ $t('Chemistry') }}
-                </router-link>
-              </li>
-              <li class="mb10">
-                <router-link :to="localizedRoute('/spozywcze')" class="cl-accent relative">
-                  {{ $t('Food') }}
-                </router-link>
-              </li>
-            </ul>
-          </nav>
-        </div>
->>>>>>> 18d61600843cc492113af9a8fdd659452e68abcf
-      </div>
-    </div>
+    <manufacturer-slider :css-class="['manufacturer-slider', 'w-100']" :manufacturers="manufacturers" />
     <main-footer />
   </div>
 </template>
 <script>
 import HeaderSimple from 'theme/components/theme/blocks/Static/HeaderSimple';
+import MainSlider from 'theme/components/core/blocks/MainSlider/MainSlider'
 import ShopInfo from 'theme/components/theme/blocks/Static/ShopInfo';
 import MainFooter from 'theme/components/core/blocks/Footer/Footer';
+import ManufacturerSlider from 'theme/components/core/blocks/MultiSlider/ManufacturerSlider';
 
 export default {
   components: {
     HeaderSimple,
     ShopInfo,
-    MainFooter
+    MainFooter,
+    ManufacturerSlider,
+    MainSlider
+  },
+  data () {
+    return {
+      manufacturers: [
+        {
+          link: 'abinbev',
+          image: '/assets/atat/loga/abinbev.png'
+        },
+        {
+          link: 'alcatel',
+          image: '/assets/atat/loga/alcatel.png'
+        },
+        {
+          link: 'alstom',
+          image: '/assets/atat/loga/alstom.png'
+        },
+        {
+          link: 'armstrong',
+          image: '/assets/atat/loga/armstrong.png'
+        },
+        {
+          link: 'astrazeneca',
+          image: '/assets/atat/loga/astrazeneca.png'
+        },
+        {
+          link: 'bayer',
+          image: '/assets/atat/loga/bayer.png'
+        },
+        {
+          link: 'cooper',
+          image: '/assets/atat/loga/cooper.png'
+        },
+        {
+          link: 'dsm',
+          image: '/assets/atat/loga/dsm.png'
+        },
+        {
+          link: 'eaton',
+          image: '/assets/atat/loga/eaton.png'
+        },
+        {
+          link: 'electrolux',
+          image: '/assets/atat/loga/electrolux.png'
+        },
+        {
+          link: 'evonik',
+          image: '/assets/atat/loga/evonik.png'
+        },
+        {
+          link: 'federalmogul',
+          image: '/assets/atat/loga/federalmogul.png'
+        },
+        {
+          link: 'finisar',
+          image: '/assets/atat/loga/finisar.png'
+        },
+        {
+          link: 'henkel',
+          image: '/assets/atat/loga/henkel.png'
+        },
+        {
+          link: 'hp',
+          image: '/assets/atat/loga/hp.png'
+        }
+      ]
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 .main_category {
   &__boxes {
     flex-wrap: wrap;

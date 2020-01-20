c814909c3556c67c03320010bfac5c4e2c101029
Marcin Sulowski <rededwins@gmail.com>
2020-01-20 22:20:30 +0000

    product status component

---
+++ src/themes/default/components/core/ProductStatus.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="product-status-wrapper">
+      <div class="status" :class="cssClasses">
+        <template v-if="status === 1">
+          <i class="material-icons">check</i>
+          <span>{{ $t('available') }}</span>
+        </template>
+        <template v-else>
+          <i class="material-icons">close</i>
+          <span>{{ $t('unavailable') }}</span>
+        </template>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    status: {
+      type: Number,
+      required: true,
+      default: 1
+    }
+  },
+
+  computed: {
+    cssClasses() {
+      return {
+        'status--available': this.status === 1,
+        'status--unavailable': this.status === 0
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~theme/css/variables/colors';
+@import '~theme/css/helpers/functions/color';
+$color-strong-cyan: color(strong-cyan);
+$color-gainsboro: color(gainsboro);
+
+.product-status-wrapper {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.status {
+  display: inline-block;
+  padding: 3px 10px;
+  border: 1px solid;
+  border-radius: 3px;
+  font-size: 14px;
+
+  &--available {
+    border-color: $color-strong-cyan;
+    color: $color-strong-cyan;
+  }
+
+  &--unavailable {
+    border-color: $color-gainsboro;
+    color: $color-gainsboro;
+  }
+
+  .material-icons {
+    font-size: 1em;
+    position: relative;
+    top: 2px;
+  }
+}
+</style>

86993a0983a1f4a2c9194e55275d42db6a69aca2
Marcin Sulowski <rededwins@gmail.com>
2020-01-26 15:29:41 +0000

    wishlist styles

--- src/themes/default/components/core/blocks/Wishlist/Wishlist.vue
+++ src/themes/default/components/core/blocks/Wishlist/Wishlist.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="wishlist cl-accent">
-    <div class="row">
-      <div class="col-md-12 end-xs">
-        <i class="material-icons p15 pointer cl-accent" @click="closeWishlist">close</i>
+    <div class="row py15">
+      <div class="col-xs-8">
+        <h2 v-if="productsInWishlist.length" class="cl-black wishlist__title px30 py10">
+          {{ $t('Wishlist') }}:
+        </h2>
+      </div>
+      <div class="col-xs end-xs">
+        <i class="material-icons p10 mr5 pointer cl-black" @click="closeWishlist">close</i>
       </div>
     </div>
-    <div class="row middle-xs px40">
-      <h2 v-if="productsInWishlist.length" class="col-xs-12 col-sm cl-accent">
-        {{ $t('Wishlist') }}
-      </h2>
-      <clear-wishlist-button v-if="productsInWishlist.length" @click="clearWishlist" class="col-xs-12 col-sm mt35 mb35 end-sm" />
+    <div class="row middle-xs px20">
+      <clear-wishlist-button v-if="productsInWishlist.length" @click="clearWishlist" class="col-xs-12 col-sm mb15 end-xs" />
     </div>
     <h4 v-if="!productsInWishlist.length" class="cl-accent ml30">
       {{ $t('Your wishlist is empty.') }}
@@ -21,8 +23,8 @@
       </router-link>
       {{ $t('to find something beautiful for You!') }}
     </div>
-    <ul class="products">
-      <product v-for="wishlistProduct in productsInWishlist" :key="wishlistProduct.id" :product="wishlistProduct" />
+    <ul class="products pl30">
+      <product v-for="wishlistProduct in productsInWishlist" :key="wishlistProduct.id" :product="wishlistProduct" class="product"/>
     </ul>
   </div>
 </template>
@@ -65,10 +67,39 @@ export default {
 
 <style lang="scss" scoped>
 @import "~theme/css/animations/transitions";
-  i {
-    opacity: 0.6;
-    &:hover {
-      opacity: 1;
+@import '~theme/css/variables/colors';
+@import '~theme/css/helpers/functions/color';
+$color-gainsboro: color(gainsboro);
+$color-black: color(black);
+
+.wishlist {
+  &__title {
+    margin: 0;
+    font-size: 24px;
+    font-weight: 400;
+  }
+}
+
+.products {
+  .product {
+    position: relative;
+    color: $color-black;
+
+    a.product__name {
+      color: $color-black;
+    }
+    
+    &:not(:last-of-type) {
+      &:after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: -30px;
+        border-bottom: 1px solid $color-gainsboro;
+        width: calc(100% + 30px);
+      }
     }
   }
+}
+
 </style>
